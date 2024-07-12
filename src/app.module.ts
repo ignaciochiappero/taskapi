a4de074c885abe4ace5386656b109ea0e9688a3e
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-07-12 17:58:54 -0300

    segundo nest env deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tasksdb'), TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/tasksdb'),
+    TasksModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(port || 3000);
 }
 bootstrap();
